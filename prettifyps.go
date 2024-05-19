ec092c43d7eef4c532e4427b0bdde51cd52a1c33
luovkle <roberto.silva.dev@protonmail.com>
2024-05-19 13:13:30 -0600

    ✨ Update prettifyps

--- prettifyps.go
+++ prettifyps.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
 	"strings"
 )
@@ -21,17 +22,25 @@ type Container struct {
 }
 
 func main() {
-	// Leer la entrada de la tubería
-	content, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		panic(err)
-	}
-
-	// Decodificar la entrada JSON
+	// Leer la entrada de la tubería línea por línea
+	reader := bufio.NewReader(os.Stdin)
 	var containers []Container
-	err = json.Unmarshal(content, &containers)
-	if err != nil {
-		panic(err)
+
+	for {
+		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+
+		var container Container
+		err = json.Unmarshal([]byte(line), &container)
+		if err != nil {
+			panic(err)
+		}
+
+		containers = append(containers, container)
 	}
 
 	// Crear la tabla
@@ -67,7 +76,16 @@ func main() {
 	}
 
 	// Imprimir la tabla
+	// Encontrar la longitud máxima de cada columna
+	maxServiceLen := 0
 	for _, row := range table {
-		fmt.Printf("%-10s%-10s%-20s\n", row[0], row[1], row[2])
+		if len(row[0]) > maxServiceLen {
+			maxServiceLen = len(row[0])
+		}
+	}
+
+	// Imprimir cada fila con formato adecuado
+	for _, row := range table {
+		fmt.Printf("%-*s %-10s %-20s\n", maxServiceLen+1, row[0], row[1], row[2])
 	}
 }
